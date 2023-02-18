450f26cdfa56443d845b7ceed5a45aba0f436254
Jinwoo Lee <starday456@kaist.ac.kr>
2023-02-18 19:32:37 +0900

    remove duplication and separate role

--- src/main/java/com/inflearn/core/AppConfig.java
+++ src/main/java/com/inflearn/core/AppConfig.java
@@ -1,6 +1,8 @@
 package com.inflearn.core;
 
+import com.inflearn.core.discount.DiscountPolicy;
 import com.inflearn.core.discount.FixDiscountPolicy;
+import com.inflearn.core.member.MemberRepository;
 import com.inflearn.core.member.MemberService;
 import com.inflearn.core.member.MemberServiceImpl;
 import com.inflearn.core.member.MemoryMemberRepository;
@@ -11,10 +13,18 @@ import com.inflearn.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
